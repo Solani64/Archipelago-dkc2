92d206a6d1ba19dabce5faef5bd6ddb8e6f12d8c
Solani64 <d.hennings@hotmail.com>
2023-09-07 15:37:22 -0700

    testing
    prep to test if this pushes to fork

--- worlds/dkc2/Names/ItemNames.py
+++ worlds/dkc2/Names/ItemNames.py
@@ -1,9 +1,12 @@
 # Junk Items
 one_up_balloon = "1-Up Balloon"
-
+two_up_balloon = "2-Up Balloon"
+three_up_balloon = "3-Up Balloon"
+#banana_coin = "Banana Coin"
 
 # Collectables
 kremkoin = "Kremkoin"
+dk_coin = "DK Coin"
 
 
 # Victory
